643726efdf88ee9dd8c4d23c5146460f278e0ddf
MityakovSA <mitykovsergey@mail.ru>
2016-09-28 17:29:11 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,13 +1,13 @@
-#include "../include/BinarySearchTree.h"
+#include "BinarySearchTree.h"
 //#include <string>
 
 int main()
 {
     BinarySearchTree<int> tree{ 8,10,3,14,6,1,7,13,4 };
     //BinarySearchTree<std::string> tree{ "ab","cd","ef" };
-    //std::ofstream out("Tree.txt");
-    //out << tree;
-    //out.close();
+    std::ofstream out("Tree.txt");
+    out << tree;
+    out.close();
     std::cout << tree << std::endl;
     //BinarySearchTree<int>* tree = new BinarySearchTree<int>();
     //std::ifstream in("Tree.txt");
