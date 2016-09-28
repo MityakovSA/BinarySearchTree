5a5f9511c7f8643960ba783f89f3a42f7d6effe3
MityakovSA <mitykovsergey@mail.ru>
2016-09-28 20:46:56 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,10 +4,22 @@
 int main()
 {
     BinarySearchTree<int> tree{ 8,10,3,14,6,1,7,13,4 };
+    //BinarySearchTree<int> tree2{ 8,10,3,14,6,1,7,13,4 };
+    //if (tree == tree2) std::cout << "TRUE" << std::endl;
+    //else std::cout << "FALSE" << std::endl;
+    //tree.insert(5);
+    //if (tree == tree2) std::cout << "TRUE" << std::endl;
+    //else std::cout << "FALSE" << std::endl;
+    //std::cin >> tree;
     //BinarySearchTree<std::string> tree{ "ab","cd","ef" };
-    std::ofstream out("Tree.txt");
-    out << tree;
-    out.close();
+    //std::ofstream out("Tree.txt");
+    //out << tree;
+    //out.close();
+    std::cout << tree << std::endl;
+    BinarySearchTree<int> tree2(tree);
+    std::cout << tree2 << std::endl;
+    tree2.insert(5);
+    tree = tree2;
     std::cout << tree << std::endl;
     //BinarySearchTree<int>* tree = new BinarySearchTree<int>();
     //std::ifstream in("Tree.txt");
